9af45babbc83e10924c0169ca11251a347a36364
So-chiru <unleash9224@gmail.com>
2021-09-20 00:12:01 +0900

    Fix json field name

--- structs/dashboard.go
+++ structs/dashboard.go
@@ -3,7 +3,7 @@ package structs
 import "github.com/so-chiru/llct-server/dashboard"
 
 type LLCTUpdate struct {
-	Updates    int64                 `json:"updateAt"`
+	Updates    int64                 `json:"updates"`
 	Notices    []dashboard.Notices   `json:"notices"`
-	Dashboards []dashboard.Dashboard `json:"dashboard"`
+	Dashboards []dashboard.Dashboard `json:"dashboards"`
 }
